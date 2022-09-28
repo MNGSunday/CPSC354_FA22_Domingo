7debfe66773a96093558ece7545e99938160ce22
Marc Domingo <mdsunday01@gmail.com>
2022-09-28 10:37:38 -0700

    updated Interpreter.hs and numbers.cf for part2

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -5,4 +5,10 @@ import AbsNumbers
 eval :: Exp -> Integer
 eval (Num n) = n
 eval (Plus n m) = (eval n) + (eval m)
+eval (Minus n m) = (eval n) - (eval m)
 eval (Times n m) = (eval n) * (eval m)
+eval (Div n m) = divide (eval n) (eval m)
+eval (Mod n m) = (eval n) % (eval m)
+eval (Pow n m) = (eval n) ^ (eval m)
+eval (Root n m) = root (eval n) (eval m)
+eval (Neg n) = -(eval n)
